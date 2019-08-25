9834d6b195663700309fa0971e5c2459e80e9a28
AlicePeng <aliceyufeipeng@gmail.com>
2019-08-25 21:43:34 +0800

    Add subcommand linecount
    - Add cmd/linecount.go
      linecount is a subcommand of root fops
    	when using `fops linecount -f filename`, it will show how many lines is there in the file
    	if the argument of -f is a directory, not exist or a binary file, responce error
    	version has two flags: file and help
    - Edit version
      use vX.X.X instead of X.X.X

---
+++ cmd/linecount.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var filename string
+
+//Add a subcommand linecount under fopd
+func init() {
+	RootCmd.AddCommand(linecountCmd)
+
+	//flag --file -f
+	linecountCmd.Flags().StringVarP(&filename, "file", "f", "", "the input file")
+	linecountCmd.MarkFlagRequired("file")
+}
+
+//Implement linecount and its function
+var linecountCmd = &cobra.Command{
+	Use:   "linecount",
+	Short: "Print line count of file",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		//check if the argument of file flag is legal: not a directory, exist, not a binary file.
+		fileinfo, err := os.Stat(filename)
+		if err != nil {
+			fmt.Printf("error:%s\n", strings.Split(err.Error(), ":")[1])
+			return nil
+		}
+		if fileinfo.IsDir() == true {
+			fmt.Printf("error: Expected file got directory '%s'\n", filename)
+			return nil
+		} else if strings.Index(fileinfo.Mode().String(), "x") != -1 {
+			fmt.Printf("error: Cannot do linecount for binary file '%s'\n", filename)
+			return nil
+		}
+
+		//read the file
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println("error: ", err)
+			return nil
+		}
+
+		//count how many lines does this file have
+		fmt.Println(len(strings.Split(string(data), "\n")))
+		return nil
+
+	},
+}
